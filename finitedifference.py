bc89298239a2f2f087329cc936731c4965a67a35
goodtimeswithpaul <moi@paultissot.ch>
2024-02-23 16:33:29 +0100

    feat: improved main function modularization and ran profiling commands

--- finitedifference.py
+++ finitedifference.py
@@ -10,7 +10,63 @@ with the finite difference method
 
 """
 
+def get_mask(N):
+	mask = np.zeros((N,N),dtype=bool)
+	mask[0,:]  = True
+	mask[-1,:] = True
+	mask[:,0]  = True
+	mask[:,-1] = True
+	mask[int(N/4):int(N*9/32),:N-1] = True
+	mask[1:N-1,int(N*5/16):int(N*3/8)] = False
+	mask[1:N-1,int(N*5/8):int(N*11/16)] = False
+
+	return mask
+
+def get_initial_U(N, mask):
+	U = np.zeros((N,N))
+	U[mask] = 0
+	return U
+
 @profile
+def compute_finite_difference(U, Uprev, mask, L, R, fac, aX, aY, xlin, tEnd, dt, plotRealTime):
+	t = 0
+	outputCount = 1
+	while t < tEnd:
+		# calculate laplacian 
+		ULX = np.roll(U, L, axis=aX)
+		URX = np.roll(U, R, axis=aX)
+		ULY = np.roll(U, L, axis=aY)
+		URY = np.roll(U, R, axis=aY)
+		
+		laplacian = ( ULX + ULY - 4*U + URX + URY )
+		
+		# update U
+		Unew = 2*U - Uprev + fac * laplacian
+		Uprev = 1.*U
+		U = 1.*Unew
+		
+		# apply boundary conditions (Dirichlet/inflow)
+		U[mask] = 0
+		U[0,:] = np.sin(20*np.pi*t) * np.sin(np.pi*xlin)**2
+		
+		# update time
+		t += dt
+		
+		# plot in real time
+		# if (plotRealTime) or t >= tEnd:
+		# 	plt.cla()
+		# 	Uplot = 1.*U
+		# 	Uplot[mask] = np.nan
+		# 	plt.imshow(Uplot.T, cmap=cmap)
+		# 	plt.clim(-3, 3)
+		# 	ax = plt.gca()
+		# 	ax.invert_yaxis()
+		# 	ax.get_xaxis().set_visible(False)
+		# 	ax.get_yaxis().set_visible(False)	
+		# 	ax.set_aspect('equal')	
+		# 	plt.pause(0.001)
+		# 	outputCount += 1
+
 def main():
 	""" Finite Difference simulation """
 	
@@ -35,63 +91,17 @@ def main():
 	Y, X = np.meshgrid( xlin, xlin )
 	
 	# Generate Initial Conditions & mask
-	U = np.zeros((N,N))
-	mask = np.zeros((N,N),dtype=bool)
-	mask[0,:]  = True
-	mask[-1,:] = True
-	mask[:,0]  = True
-	mask[:,-1] = True
-	mask[int(N/4):int(N*9/32),:N-1]     = True
-	mask[1:N-1,int(N*5/16):int(N*3/8)]  = False
-	mask[1:N-1,int(N*5/8):int(N*11/16)] = False
-	U[mask] = 0
+	mask = get_mask(N)
+	U = get_initial_U(N, mask)
 	Uprev = 1.*U
 
 	# prep figure
 	# fig = plt.figure(figsize=(6,6), dpi=80)
 	# cmap = plt.cm.bwr
 	# cmap.set_bad('gray')
-	# outputCount = 1
-	
-	# Simulation Main Loop
-	while t < tEnd:
-		
-		# calculate laplacian 
-		ULX = np.roll(U, L, axis=aX)
-		URX = np.roll(U, R, axis=aX)
-		ULY = np.roll(U, L, axis=aY)
-		URY = np.roll(U, R, axis=aY)
-		
-		laplacian = ( ULX + ULY - 4*U + URX + URY )
-		
-		# update U
-		Unew = 2*U - Uprev + fac * laplacian
-		Uprev = 1.*U
-		U = 1.*Unew
-		
-		# apply boundary conditions (Dirichlet/inflow)
-		U[mask] = 0
-		U[0,:] = np.sin(20*np.pi*t) * np.sin(np.pi*xlin)**2
-		
-		# update time
-		t += dt
-		
-		# plot in real time
-		# if (plotRealTime) or (t >= tEnd):
-		# 	plt.cla()
-		# 	Uplot = 1.*U
-		# 	Uplot[mask] = np.nan
-		# 	plt.imshow(Uplot.T, cmap=cmap)
-		# 	plt.clim(-3, 3)
-		# 	ax = plt.gca()
-		# 	ax.invert_yaxis()
-		# 	ax.get_xaxis().set_visible(False)
-		# 	ax.get_yaxis().set_visible(False)	
-		# 	ax.set_aspect('equal')	
-		# 	plt.pause(0.001)
-		# 	outputCount += 1
-			
-	
+
+	compute_finite_difference(U, Uprev, mask, L, R, fac, aX, aY, xlin, tEnd, dt, plotRealTime)
+				
 	# Save figure
 	# plt.savefig('finitedifference.png',dpi=240)
 	# plt.show()
